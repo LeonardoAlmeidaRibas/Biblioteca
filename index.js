c41a38c89ad81e93a0b1f42d2ad6b560ffa9afa2
LeonardoAlmeidaRibas <leo.almeidaribas21@gmail.com>
2024-09-04 21:31:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const express = require("express")
-const estudante_router = require("./routes/estudante.js")
-const aluguel_router = require("./routes/aluguel.js")
-const livro_router = require("./routes/livro.js")
-const app = express()
-const port = 5000
+const express = require("express");
 
-app.use(express.json())
+const app = express();
+const port = 5000;
 
-app.use("/aluguel", aluguel_router)
-app.use("/livro", livro_router)
-app.use("/estudante", estudante_router)
+app.use(express.json());
+// app.use(cep_endereco) // Middleware de uso global
 
 app.listen(port, () => {
-    console.log(`Server running in ${port} port`)
-})
+    console.log(`Server running in ${port} port`);
+});
+
+const estudante_router = require("./routes/estudante.js");
+const livro_router = require("./routes/livro.js");
+const aluguel_router = require("./routes/aluguel.js");
+
+app.use("/Livro", livro_router);
+app.use("/Estudante", estudante_router);
+app.use("/Aluguel", aluguel_router);
